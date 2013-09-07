05d86384ae65a314a1dab9d8bb19a7adaee80339
Myth <kenlee.myth@gmail.com>
2013-09-07 11:27:49 +0800

    配置文件
    对配置文件进行了修改数组的错误问题

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
  +----------------------------------------------------------------------
  | MythPHP框架 [佛山市德信创易网络有限公司]
  +----------------------------------------------------------------------
- | Copyright (c) 2013 - 2113 http://www.dexinnet.com All rights reserved.
+ | Copyright (c) 2013 - 2063 http://www.dexinnet.com All rights reserved.
  +----------------------------------------------------------------------
- | Author: 李勇 <e-mail:myth@dexinnet.com> <QQ:5849963>
+ | Author: 李勇 <E-mail:myth@dexinnet.com> <QQ:5849963>
  +----------------------------------------------------------------------
  | 文件功能：框架配置文件
  +----------------------------------------------------------------------
@@ -14,7 +14,7 @@
 
 defined('ACCESS')||exit('Access Denied');
 
-$c = $array();
+$c = array();
 
 //版本信息号
 $c['version'] = "MythPHP1.0"; //系统版本号
@@ -26,11 +26,11 @@ $c['timezone'] = 'Asia/Hong_Kong'; // 时区 PRC
 
 //数据库配置
 $c['dbhost']        ="localhost";		//配置主机
-$c['dbuser']		="mishi";			//数据库用户
-$c['dbpw']			="J3bL3cMY";		//数据库密码
-$c['dbname']		="sqlmishi";		//数据库密码
+$c['dbuser']		="root";			//数据库用户
+$c['dbpw']			="";		//数据库密码
+$c['dbname']		="wogoule";		//数据库密码
 $c['dbcharset'] 	="utf8";			//数据库字符
-$c['dbpre']         ="xroom_";			//数据库表名前缀
+$c['dbpre']         ="wgl_";			//数据库表名前缀
 
 //程序调试模式
 $c['debug'] = TRUE;  //程序调试模式开启
